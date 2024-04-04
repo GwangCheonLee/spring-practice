7ed6c9db05099b085199fc5e80910177bbf6b357
gclee <raitmid56@gmail.com>
2024-04-04 20:07:16 +0900

    Add PacmanGame class

--- src/main/java/com/momentslee/learnspringframework/AppGamingBasicJava.java
+++ src/main/java/com/momentslee/learnspringframework/AppGamingBasicJava.java
@@ -1,11 +1,12 @@
 package com.momentslee.learnspringframework;
 
 import com.momentslee.learnspringframework.game.GameRunner;
-import com.momentslee.learnspringframework.game.MarioGame;
+import com.momentslee.learnspringframework.game.PacmanGame;
 
 public class AppGamingBasicJava {
     public static void main(String[] args) {
-        var game = new MarioGame();
+        var game = new PacmanGame();
+//        var game = new MarioGame();
 //        var game = new SuperContraGame();
         var gameRunner = new GameRunner(game);
         gameRunner.run();
