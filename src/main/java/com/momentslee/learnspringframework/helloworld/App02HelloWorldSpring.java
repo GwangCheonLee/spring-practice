591f47a4350964b1e1dc1c6164ee4c4eeb045ee6
gclee <raitmid56@gmail.com>
2024-04-13 11:44:03 +0900

    Moved App02HelloWorldSpring.java and HelloWorldConfiguration.java to helloworld folder

--- src/main/java/com/momentslee/learnspringframework/helloworld/App02HelloWorldSpring.java
+++ src/main/java/com/momentslee/learnspringframework/helloworld/App02HelloWorldSpring.java
@@ -1,4 +1,4 @@
-package com.momentslee.learnspringframework;
+package com.momentslee.learnspringframework.helloworld;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
